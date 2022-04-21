e9562933d7c115cb77436935795cd52c7b9c179c
Arjen Tienkamp <atienkamp@gmail.com>
2022-04-21 19:24:07 +0200

    Update tslint.json

--- projects/ngx-mat-calendar/src/lib/components/shared/event-display/event-display.directive.ts
+++ projects/ngx-mat-calendar/src/lib/components/shared/event-display/event-display.directive.ts
@@ -1,7 +1,6 @@
 import { Directive, ViewContainerRef } from '@angular/core';
 
 @Directive({
-  // tslint:disable-next-line:directive-selector
   selector: '[eventDisplay]'
 })
 export class EventDisplayDirective {
